c694680783519b43948d456db2696e0594b1498f
김준호 <92168240+kokohuhusave@users.noreply.github.com>
2024-02-19 14:34:51 +0900

    Delete src directory

--- src/main/java/com/shop/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shop.repository;
-
-import com.shop.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUser_Id(Long userId); // 메소드 이름 변경
-}
--- src/main/java/com/shop/shopservice/OrderItemService.java
+++
@@ -1,6 +0,0 @@
-package com.shop.shopservice;
-
-import com.shop.entity.OrderItem;
-
-public interface OrderItemService<T, U> extends CommonService<T, U> {
-}
